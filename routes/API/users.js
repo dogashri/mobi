fc0b24833b475d2b8307088a352de89548dc6e59
rishabh <rishabh singh>
2020-08-12 00:47:56 +0530

    .

--- routes/API/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcryptjs')
-const config = require('config')
-const {check,validationResult} = require('express-validator');
-const User = require('../../models/User');
-const Jwt = require('jsonwebtoken');
-
